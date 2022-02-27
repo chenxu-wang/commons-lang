d3dcf087420d53bb545690d5e3952013a3959c5b
chenxu-wang <chenxuw4@uci.edu>
2022-02-27 00:12:24 -0800

    update mockito test

--- src/test/java/org/apache/commons/lang3/SWE261_P5_Test.java
+++ src/test/java/org/apache/commons/lang3/SWE261_P5_Test.java
@@ -1,44 +1,38 @@
 package org.apache.commons.lang3;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.apache.commons.lang3.concurrent.*;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class SWE261_P5_Test {
-    StringUtils strutils;
-    @Before
-    public void setup() {
-        //  Mocks are being created.
-        strutils = (StringUtils) Mockito.mockStatic(StringUtils.class);
-
-        MockitoAnnotations.initMocks(this);
+    @Test
+    public void testConcurrent() throws ConcurrentException {
+        final ConcurrentInitializer<Object> init = mock(ConcurrentInitializer.class);
+        final Object result = new Object();
+        when(init.get()).thenReturn(result);
+        assertThat(ConcurrentUtils.initialize(init)).isEqualTo(result);
+        verify(init).get();
     }
     @Test
-    public void testIsAlpha() {
-
-        try (MockedStatic<StringUtils> utilities = Mockito.mockStatic(StringUtils.class)) {
-            utilities.when(() -> strutils.isAlpha(""))
-                    .thenReturn(false);
-            utilities.verify(()->StringUtils.isAlpha(""), times(0));
-        }
-     assertThat(StringUtils.isAlpha("")).isFalse();
-
-
+    public void testConcurrentEx() throws ConcurrentException {
+        final
+        ConcurrentInitializer<Object> init = mock(ConcurrentInitializer.class);
+        final Exception cause = new Exception();
+        when(init.get()).thenThrow(new ConcurrentException(cause));
+        final ConcurrentRuntimeException crex =
+                assertThrows(ConcurrentRuntimeException.class, () -> ConcurrentUtils.initializeUnchecked(init));
+        assertThat(cause).isEqualTo(crex.getCause());
+        verify(init).get();
     }
-    @Test
-    public void testArrayUtil() {
-       int[] data = new int[]{1,2,3};
-        try (MockedStatic<ArrayUtils> utilities = Mockito.mockStatic(ArrayUtils.class)) {
-            utilities.when(() -> ArrayUtils.add(data,4))
-                    .thenReturn(new int[]{1,2,3,4});
-            utilities.verify(()->ArrayUtils.add(data,4), times(0));
-        }
-        assertThat(ArrayUtils.add(data,4)).containsExactly(1,2,3,4);
-    }
+
 }
